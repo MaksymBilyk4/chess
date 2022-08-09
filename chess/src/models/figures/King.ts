93e1b44aefbccf323153d15f5ed99a607be44c6f
Max Bilyk <datan0612@gmail.com>
2022-08-09 19:34:51 +0300

    figures movement

--- chess/src/models/figures/King.ts
+++ chess/src/models/figures/King.ts
@@ -11,4 +11,11 @@ export class King extends Figure {
         this.logo = color === Colors.WHITE ? whiteLogo : blackLogo;
         this.name = FigureNames.KING;
     }
+
+    canMove(target: Cell): boolean {
+        if (!super.canMove(target)) {
+            return false;
+        }
+        return true;
+    }
 }
